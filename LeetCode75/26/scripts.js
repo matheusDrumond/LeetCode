5462fec3960910c665009f102f23e2efcbce597c
matheusDrumond <drumond.matheusc@gmail.com>
2024-05-07 20:47:23 -0300

    Resolution of problem 26 in 'LeetCode 75'

--- LeetCode75/26/scripts.js
+++ LeetCode75/26/scripts.js
@@ -11,19 +11,18 @@ var decodeString = function(s) {
         // if the current character is not ']', push it onto the stack
         if (char !== ']') {
             stack.push(char);
-        } else {
-            // if the current character is ']', we'll start decoding the string
-            
+        }    
+        // if the current character is ']', we'll start decoding the string
+        else {
+
             // declaring an empty string to store the encoded part
-            let encodedString = '';
-            
+            let encodedString = '';      
             // pop characters from the stack until '[' is encountered.
             let currentChar = stack.pop();
             while (currentChar !== '[') {
                 encodedString = currentChar + encodedString;
                 currentChar = stack.pop();
             }
-            
             // extract the number 'k' from the stack.
             let num = '';
             currentChar = stack.pop();
@@ -31,13 +30,11 @@ var decodeString = function(s) {
                 num = currentChar + num;
                 currentChar = stack.pop();
             }
-            
             // push the decoded string 'k[encoded_string]' back onto the stack.
             stack.push(currentChar); // push the character before '['
             stack.push(encodedString.repeat(parseInt(num))); // push the decoded string repeated 'k' times.
         }
     }
-    
     // join the characters remaining in the stack to form the decoded string and return it.
     return stack.join('');
 };
